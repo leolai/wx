9fa745f74679ec526408c293be609ad19ba65cf5
leolai <kakujing@gmail.com>
2015-11-06 18:18:45 +0800

    'del'

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  return [
-  		'encodingAesKey' 	=>	"JFRZiYDeQu8fJf3UMFsZfQ3Ck3HK38DbIrBLYuIwpcw",
-  		'token' 			=>	"RkoOOEFuLMrA0NSmwHrgwVFPX6GJI3ME",
-  		'appId' 			=>	"wxace52c44ad0af92d",
-  		'debug' 			=>	TRUE,
-  ];
-
