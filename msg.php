9e1cbd7ec3ae50c42bd01ff789a962087ceb6e2a
leolai <kakujing@gmail.com>
2015-11-13 16:53:55 +0800

    '1113'

---
+++ msg.php
@@ -0,0 +1,2 @@
+<?php
+file_put_contents(__DIR__ . 'testmsg', $_REQUEST);
---
+++ src/config.php
@@ -0,0 +1,9 @@
+<?php
+  return [
+  		'encodingAesKey' 	=>	"JFRZiYDeQu8fJf3UMFsZfQ3Ck3HK38DbIrBLYuIwpcw",
+  		'token' 			=>	"RkoOOEFuLMrA0NSmwHrgwVFPX6GJI3ME",
+  		'appId' 			=>	"wxace52c44ad0af92d",
+  		'debug' 			=>	TRUE,
+  		'default'			=>	'鱼鱼，我爱你！',
+  ];
+
